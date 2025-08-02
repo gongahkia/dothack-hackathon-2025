a00545e592760479d56a49f5b692ce4280b69d10
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2025-08-02 11:09:19 +0800

    feat(app): ADddinf firebase secrets

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, get } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "https://your-db.firebaseio.com",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBa1zLJLRngGQCC0A-kRQNyfl2UG232kx8",
+  authDomain: "quizbatter1.firebaseapp.com",
+  databaseURL: "https://quizbatter1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizbatter1",
+  storageBucket: "quizbatter1.firebasestorage.app",
+  messagingSenderId: "998121763396",
+  appId: "1:998121763396:web:0c50b1a4889fdcd2d4c211",
+  measurementId: "G-X5B6ERZMP2"
 };
 
 const app = initializeApp(firebaseConfig);
